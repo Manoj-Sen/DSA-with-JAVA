53a40dfdbfef51ece59f680ee0be8273f46217a7
Manoj Kumar Sen <112579743+Manoj-Sen@users.noreply.github.com>
2023-12-20 18:36:43 +0530

    Modified Game of  Nim.java

---
+++ Modified Game of  Nim.java
@@ -0,0 +1,12 @@
+class Solution{
+    static int findWinner(int n, int A[]){
+        int num = 0;
+        for(int i=0;i<n;i++){
+            num^=A[i];
+        }
+        
+        if(num==0)  return 1;
+        if(n%2==0) return 1;
+        return 2;
+    }
+}
